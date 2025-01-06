ac2b9a2490fa9bb9d94092382a8b2e17ce9d791b
ththien0223@gmail.com <tht252003@gmail.com>
2025-01-06 20:51:12 +0700

    prepare-deploy

--- backend/index.js
+++ backend/index.js
@@ -13,33 +13,41 @@ import { app, server } from "./app/socket/socket.js";
 // authen middleware
 // import checkToken from "./app/authentication/auth.js";
 // CORS configuration
-const corsOptions = {
-  origin: "http://localhost:4200", // Allow requests from Angular application
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
 
-// Get the filename and directory name in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
+// const corsOptions = {
+//   origin: "http://localhost:4200", // Allow requests from Angular application
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
+//   allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+// };
+
+
+dotenv.config();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
-dotenv.config();
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
-
-//app.use("/static", express.static(path.join(__dirname, "public")));
-app.use("/static", express.static(path.join(__dirname, "assets/files")));
-// app.use(checkToken);
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+// app.use(cors(corsOptions));
 
 //connect();
 route(app);
+
+//app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+// app.use(checkToken);
+// app.use(express.json({ limit: "50mb" }));
+// app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+
 server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server started at port ${PORT}`);
